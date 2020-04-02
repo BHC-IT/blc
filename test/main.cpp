54a3bcf2cca950b613c343b0a4121fc3d8e4b9ab
lanath <valentin.vivier@yahoo.fr>
2020-04-02 15:28:30 +0200

    removed some useless tests

--- test/main.cpp
+++ test/main.cpp
@@ -10,9 +10,5 @@ unsigned int Factorial( unsigned int number ) {
 
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
-	REQUIRE( Factorial(0) == 0 );
-	REQUIRE( Factorial(1) == 1 );
-	REQUIRE( Factorial(2) == 2 );
 	REQUIRE( Factorial(3) == 6 );
-	REQUIRE( Factorial(10) == 3628800 );
 }
