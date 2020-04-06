ecc2f54386cfa1654fe5529ac43f715e5d0a4941
lanath <valentin.vivier@yahoo.fr>
2020-04-06 20:13:01 +0200

    correction for windows

--- src/socket.cpp
+++ src/socket.cpp
@@ -251,7 +251,11 @@ bool blc::network::Socket::writable() const {
 
 bool blc::network::Socket::waitRead(unsigned int usec) const {
 	fd_set fds;
-	struct timeval timeout = {0, reinterpret_cast<unsigned int>(usec)};
+	#ifdef __WIN32
+		struct timeval timeout = {0, static_cast<long int>(usec)};
+	#else
+		struct timeval timeout = {0, reinterpret_cast<unsigned int>(usec)};
+	#endif
 
 	FD_ZERO(&fds);
 	FD_SET(this->_socket, &fds);
