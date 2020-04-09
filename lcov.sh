c76b40aca499b3750a943e29a6821f092fd581f4
lanath <valentin.vivier@yahoo.fr>
2020-04-09 20:09:48 +0200

    added /coverage/lcov.info

--- lcov.sh
+++ lcov.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 #init lcov
-lcov --no-external -c -i -d ./build/CMakeFiles -o ./build/cov.info
+lcov -c -i -d ./build/CMakeFiles -o ./build/cov.info
 
 #run unit test executable
 ./build/test
 
 #capture coverage after running executable
-lcov --no-external -c -d ./build/CMakeFiles -o ./build/cov.info
+lcov --exclude '*test/*' --exclude '/usr/*' -c -d ./build/CMakeFiles -o ./coverage/lcov.info
+# lcov --remove ./build/cov.info '/usr/*' --output-file ./build/cov.info
+# lcov --remove ./build/cov.info './test/*' --output-file ./build/cov.info
 
 # I added this in- Excludes some third party code
 # lcov --remove $TARGET_DIR/cov.info '/opt/*' '/usr/*' '$ROOT_DIR/Common?ExternalLibraries/*'
 
 #I added this in-generate report
-genhtml ./build/cov.info -o ./build/out
+genhtml ./coverage/lcov.info -o ./build/out
