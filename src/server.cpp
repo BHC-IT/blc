cde39de42d9ff84ae02ef0e1ea27f640dcafaa3d
lanath <valentin.vivier@yahoo.fr>
2020-04-14 13:08:16 +0200

    added more precise error info to debug mac

--- src/server.cpp
+++ src/server.cpp
@@ -49,16 +49,16 @@ blc::network::Server::Server(unsigned int maxClient, unsigned int port, bool blo
 														_type(type) {
 	if (block) {
 		if ((this->_sock = ::socket(this->_type, this->_mode, 0)) == -1)
-			throw blc::error::exception(strerror(errno));
+			throw blc::error::exception(traceError(strerror(errno)));
 	} else {
 		if ((this->_sock = ::socket(this->_type, this->_mode | SOCK_NONBLOCK, 0)) == -1)
-			throw blc::error::exception(strerror(errno));
+			throw blc::error::exception(traceError(strerror(errno)));
 	}
 	this->_addr.sin_family = this->_type;
 	this->_addr.sin_port = htons(port);
 	this->_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	if (::bind(this->_sock, (const struct sockaddr *)&this->_addr, sizeof(struct sockaddr_in)) == -1)
-		throw blc::error::exception(strerror(errno));
+		throw blc::error::exception(traceError(strerror(errno)));
 	this->listen();
 }
 
